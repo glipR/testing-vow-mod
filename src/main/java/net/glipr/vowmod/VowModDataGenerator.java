67b97147fb432d7ba167019a4eccb2dd15946340
Jackson Goerner <37640160+glipR@users.noreply.github.com>
2024-02-11 09:42:05 +1100

    Initial commit

---
+++ src/main/java/net/glipr/vowmod/VowModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.glipr.vowmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VowModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
